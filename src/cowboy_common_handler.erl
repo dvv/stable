1be0dfb28b989c3d38103d2d9f265a503b57b67a
Vladimir Dronnikov <dronnikov@gmail.com>
2013-03-25 14:46:23 +0400

    simplify common handler

--- src/cowboy_common_handler.erl
+++ src/cowboy_common_handler.erl
@@ -1,14 +1,16 @@
-%%
-%% @doc Delegates actual processing to a helper module
-%%      which exposes `handler(Method, Path, Req, Session)`.
-%%      Overwrites session if handler returns modified session.
-%%
-%% @doc Return values are matched against the common cases:
-%%      - status, headers and body known
-%%      - error status known
-%%      - render a module to HTML with some data
-%%      - response already answered
-%%
+%%% ----------------------------------------------------------------------------
+%%%
+%%% @doc Provide quick coding patterns for some common use cases by delegating
+%%%      actual processing to a helper module which exposes
+%%%      `action(Method, Path, Req)` function.
+%%%
+%%% @doc The following cases covered:
+%%%      - status, headers and body known
+%%%      - status known
+%%%      - redirect to new location
+%%%      - render a module to HTML with some data
+%%%
+%%% ----------------------------------------------------------------------------
 
 -module(cowboy_common_handler).
 -author('Vladimir Dronnikov <dronnikov@gmail.com>').
@@ -24,74 +26,34 @@ init(_Transport, Req, Opts) ->
 terminate(_Reason, _Req, _State) ->
   ok.
 
-handle(Req, Opts) ->
+handle(Req, State) ->
   % get request method and path
   [Method, Path] = cowboy_req:get([method, path_info], Req),
 
-  % get session
-  {Session, Req2} = case lists:keyfind(session_opts, 1, Opts) of
-    {_, SessionOpts} ->
-      cowboy_cookie_session:get_session(SessionOpts, Req);
-    false ->
-      SessionOpts = undefined,
-      {[], Req}
-  end,
-
   % call handler
-  {_, Handler} = lists:keyfind(handler, 1, Opts),
-  case Handler:handler(Method, Path, Req2, Session) of
+  {_, Handler} = lists:keyfind(handler, 1, State),
+  Req2 = Req,
+  case Handler:action(Method, Path, Req2) of
     % handler already responded
     {ok, Req3} ->
       {ok, Req3, undefined};
     % status, headers, body known
-    {Status, Headers, Body, Req3, Session2} when is_integer(Status) ->
-      respond(Status, Headers, Body, Req3, Session, Session2, SessionOpts);
     {Status, Headers, Body, Req3} when is_integer(Status) ->
-      respond(Status, Headers, Body, Req3);
+      {ok, Req4} = cowboy_req:reply(Status, Headers, Body, Req3),
+      {ok, Req4, undefined};
     % shortcut response: status known, no body
-    {Status, Req3, Session2} when is_integer(Status) ->
-      respond(Status, [], <<>>, Req3, Session, Session2, SessionOpts);
     {Status, Req3} when is_integer(Status) ->
-      respond(Status, [], <<>>, Req3);
-    % redirect
-    {redirect, Location, Req3, Session2} ->
-      respond(302, [
-          {<<"location">>, Location}
-        ], <<>>, Req3, Session, Session2, SessionOpts);
+      {ok, Req4} = cowboy_req:reply(Status, Req3),
+      {ok, Req4, undefined};
+    % redirect and new location
     {redirect, Location, Req3} ->
-      respond(302, [
+      {ok, Req4} = cowboy_req:reply(302, [
           {<<"location">>, Location}
-        ], <<>>, Req3);
+        ], <<>>, Req3),
+      {ok, Req4, undefined};
     % template module and variables for interpolation
-    {render, TemplateModule, Data, Req3, Session2} ->
-      render(TemplateModule, Data, Req3, Session, Session2, SessionOpts);
     {render, TemplateModule, Data, Req3} ->
-      render(TemplateModule, Data, Req3, false, false, undefined)
+      {ok, IoList} = TemplateModule:render(Data),
+      {ok, Req4} = cowboy_req:reply(200, [], IoList, Req3),
+      {ok, Req4, undefined}
   end.
-
-render(TemplateModule, Data, Req, Session, Session2, SessionOpts) ->
-  try TemplateModule:render(Data) of
-    {ok, IoList} ->
-      respond(200, [
-          {<<"content-type">>, <<"text/html; charset=UTF-8">>}
-        ], IoList, Req, Session, Session2, SessionOpts);
-    {error, _Error} ->
-      respond(500, [], <<"Render error">>, Req)
-  catch _:_ ->
-    respond(500, [], <<"Render error">>, Req)
-  end.
-
-respond(Status, Headers, Body, Req) ->
-  {ok, Req2} = cowboy_req:reply(Status, Headers, Body, Req),
-  {ok, Req2, undefined}.
-
-respond(Status, Headers, Body, Req, _, _, undefined) ->
-  respond(Status, Headers, Body, Req);
-respond(Status, Headers, Body, Req, Session, Session2, SessionOpts) ->
-  Req2 = case Session2 of
-    Session ->
-      Req;
-    _ ->
-      cowboy_cookie_session:set_session(Session2, SessionOpts, Req)
-  end,
-  respond(Status, Headers, Body, Req2).
