62cdb5a9aa5a23dee98c6732a6865848553b8b0e
rambocoder <erlang@rambocoder.com>
2014-04-04 13:40:48 -0400

    Remove io:format and converted chars() into binary()

--- src/cowboy_csrf.erl
+++ src/cowboy_csrf.erl
@@ -23,7 +23,7 @@ execute(Req0, Env0) ->
   	  % check if CSRF token is in body, query string, header
   	  case csrf_from_body(Req1) of
   	  	{undefined, Req2} -> 
-			{ok, Req3} = cowboy_req:reply(403, [], "Body does not contain CSRF Token.", Req2),			
+			{ok, Req3} = cowboy_req:reply(403, [], <<"Body does not contain CSRF token.">>, Req2),			
 			{error, 403, Req3};
   	  	{error, _Reason} -> {error, 500, Req1};
   	  	{CSRFTokenValue, Req2} -> check_session_first(CSRFTokenValue, Req2, Env0)
@@ -33,7 +33,6 @@ execute(Req0, Env0) ->
 check_session_first(CSRFTokenValue, Req2, Env0) ->
 	case cowboy_session:get(Req2) of
 	  	{undefined, Req3} -> 
-	  		io:format("No session in current request. Let's create a new one.~n",[]),
 	  		Req4 = cowboy_session:set([], Req3),
 			found_session(CSRFTokenValue, [], Req4, Env0);
 		{Session, Req3} -> 
@@ -41,7 +40,6 @@ check_session_first(CSRFTokenValue, Req2, Env0) ->
 	end.
 
 found_session(CSRFTokenValue, Session, Req3, Env0)	->
-	io:format("Found session~n",[]),
 	case proplists:get_value(csrf_token, Session, undefined) of
 		undefined -> 
 			% no csrf_token in session found
@@ -49,14 +47,12 @@ found_session(CSRFTokenValue, Session, Req3, Env0)	->
 			NewToken = base64:encode(crypto:strong_rand_bytes(32)),
 			Session2 = Session ++ {csrf_token, NewToken},
 			cowboy_session:set(Session2, Req3),
-			Req4 = cowboy_req:set_resp_body("Session was invalid. It did not contain CSRF", Req3),
+			Req4 = cowboy_req:set_resp_body(<<"Session was invalid. It did not contain CSRF token.">>, Req3),
 			{error, 403, Req4};
 		CSRFTokenValue ->
-			io:format("Session CSRF matches body CSRF ~n",[]),
 			{ok, Req3, Env0};
 		SessionCSRFTokenValue -> 
-			Reason = io_lib:format("Session CSRF ~p did not match body CSRF ~p",[SessionCSRFTokenValue, CSRFTokenValue]),
-			io:format("~p~n", [Reason]),
+			Reason = io_lib:format(<<"Session CSRF token: ~p did not match body CSRF token: ~p">>,[SessionCSRFTokenValue, CSRFTokenValue]),
 			Req4 = cowboy_req:set_resp_body(Reason, Req3),
 			{error, 403, Req4}
 	end.
@@ -71,9 +67,7 @@ csrf_from_body(Req0) ->
 			Req2   = cowboy_req:set([{buffer, Buffer}], Req1),
 			Req3   = cowboy_req:set([{body_state, waiting}], Req2),
 			case proplists:get_value(<<"_csrf">>, BodyQs, undefined) of
-				undefined -> 
-					io:format("No _csrf in body, check the querystring~n", []),
-					csrf_from_querystring(Req3);
+				undefined -> csrf_from_querystring(Req3);
 				TokenValue -> {TokenValue, Req3}
 			end
 	end.
